4c91ee67fdbc3c052955e20aea4e952d6fbc612a
samu8614 <samu8614b@gmail.com>
2026-06-18 14:44:06 +0200

    started cool analysis with setosa

--- main_script.R
+++ main_script.R
@@ -12,3 +12,11 @@ setosa_subset <- iris_data |>
 
 # cool analysis
 model_setosa <- lm(Sepal.Length ~ Petal.Length, data = setosa_subset)
+
+
+# plotting
+setosa_plot <- ggplot(setosa_subset, aes(x = Petal.Length, y = Sepal.Length)) +
+  geom_point(aes(size = Petal.Width)) +
+  stat_smooth(method = "lm") +
+  theme_bw()
+
