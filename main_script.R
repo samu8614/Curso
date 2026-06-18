2719f6a8ee137befd47bd3e2927fdad002c6681b
samu8614 <samu8614b@gmail.com>
2026-06-18 14:28:04 +0200

    started cool analysis with setosa

--- main_script.R
+++ main_script.R
@@ -8,3 +8,7 @@ iris_data <- read_csv('raw_data/iris.csv')
 # subsetting data
 setosa_subset <- iris_data |>
   filter(Species == "setosa")
+
+
+# cool analysis
+model_setosa <- lm(Sepal.Length ~ Petal.Length, data = setosa_subset)
